114811cd44d8b5e1b5b3afecbecbae870ddfc2ef
Honoka Kobayashi <hononyanpo@gmail.com>
2025-06-20 00:28:19 +0900

    feat: refactoring

--- app/Preprocess.hs
+++ app/Preprocess.hs
@@ -7,29 +7,7 @@ import System.IO ()
 import qualified Data.Map.Strict as Map
 
 import TreeBuilder (buildTreeSafe, buildParentChildMap)
-
-parseEdge :: 
-  String ->  -- "parent child"
-  (String, String) -- (parent, child)
-parseEdge line =
-  let ws = words line
-  in (head ws, ws !! 1)
-
-loadEdges :: 
-  FilePath -> -- path to the edges file
-  IO [(String, String)] -- load edges from the file
-loadEdges path = do
-  contents <- readFile path
-  return $ map parseEdge (filter (not . null) (lines contents))
-
-loadTree :: 
-  FilePath -> -- path to the edges file
-  IO (Tree String) -- load the tree structure from the edges file
-loadTree path = do
-  pairs <- loadEdges path
-  let cmap = buildParentChildMap pairs
-      tree = buildTreeSafe cmap "entity"
-  return tree
+import PreprocessUtils (loadTree)
 
 main :: IO ()
 main = do
