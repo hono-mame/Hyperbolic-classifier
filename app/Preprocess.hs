582aecedc9edebd2fbbf109626c2ddc2275e5a90
Honoka Kobayashi <hononyanpo@gmail.com>
2025-06-20 14:54:47 +0900

    feat: check distance calculate function with test data.

--- app/Preprocess.hs
+++ app/Preprocess.hs
@@ -3,21 +3,29 @@
 module Preprocess (main) where
 
 import Data.Tree (Tree(..), drawTree)
-import TreeUtils (buildTreeSafe, buildParentChildMap, buildParentMap, loadEdges, distanceBetween)
 import qualified Data.Map.Strict as Map
+import TreeUtils (buildTreeSafe, buildParentChildMap, buildParentMap, loadEdges, distanceBetween)
+import FileExport (saveTreeAsDOT, saveTreeAsPNG) 
+
 
 main :: IO ()
 main = do
-  pairs <- loadEdges "data/tree.edges"
+  pairs <- loadEdges "data/tree_test.edges"
+  -- pairs <- loadEdges "data/tree.edges"
   let cmap = buildParentChildMap pairs
       pmap = buildParentMap pairs
       tree = buildTreeSafe cmap "entity"
 
+  -- for test
+  saveTreeAsDOT "data/tree_test.dot" tree
+  saveTreeAsPNG "data/tree_test.dot" "data/tree_test.png"
+  putStrLn "Tree saved as PNG."
+
   -- putStrLn "Tree structure:"
   -- putStrLn $ drawTree tree
 
-  let nodeA = "チェッカー"
-      nodeB = "アタッチメント"
+  let nodeA = "科学"
+      nodeB = "N"
 
   case distanceBetween pmap nodeA nodeB of
     Left errMsg -> putStrLn $ "error: " ++ errMsg
